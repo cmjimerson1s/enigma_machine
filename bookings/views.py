3e95316e6e272d153cfb3590eb42ff098b076fb1
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-03 15:09:58 +0000

    Add basic db query functionality to reservation

--- bookings/views.py
+++ bookings/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import generic, View
 from .models import Reservation
+from datetime import datetime, date
 
 
 def reservation_view(request):
+    date_today = date(2023, 5, 1)
     reservations = Reservation.objects.all()
-    return render(request, 'reservation.html', {'reservations': reservations})
+    return render(request, 'reservation.html', {'reservations': reservations, 'date_today': date_today})
