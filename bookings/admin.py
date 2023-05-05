c48615e21e39968761be1a7537014be5de6f5a17
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-05 13:56:59 +0000

    Cleaned views.py file and html

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Reservation, Room
+from .models import Reservation, Room, GameTime
 
 admin.site.register(Reservation)
 admin.site.register(Room)
+admin.site.register(GameTime)
 
--- bookings/views.py
+++ bookings/views.py
@@ -12,7 +12,7 @@ def dateChoiceView(request):
 
 
 def resultView(request):
-    template = "page1.html"
+    template = "reservation.html"
     specific_date = request.POST['selection']
     specific_times = GameTime.objects.all()
     room_list = Room.objects.all()
@@ -27,7 +27,7 @@ def resultView(request):
 
 def checkRes(room, date, time):
 
-    if (Reservation.objects.filter(date=date, room=room, time=time).exists()):
+    if (Reservation.objects.filter(date=date, room_choice=room, time_slot=time).exists()):
         result = 45
         return result
     else:
@@ -50,77 +50,3 @@ def dayView(rooms, date, times):
         list_pairs = {room: time_list}
         result.append(list_pairs)
     return result          
-
-def reservationGridView(request):
-    data = request.POST['selection']
-    template = 'reservation.html'
-    context = {
-        'selection': data
-    }
-    request.session['selection'] = data
-    return render(request, template, context)
-
-    times = [
-        "12:00", "14:00", "16:00", "18:00"
-    ]
-
-    status = checkTimeSlot(times, data)
-    print(status)
-    return render(request, template, {
-        'times': status,
-        'day': day,
-    })
-
-# def resView(request):
-#     template = 'reservation.html'
-#     data = request.POST['selection']
-#     request.session['selection'] = data
-
-#     times = [
-#         "12:00", "14:00", "16:00", "18:00"
-#     ]
-    
-#     results = []
-#     rooms = Room.objects.all()
-    
-    
-#     status = checkTimeSlot(times, data, rooms, results)
-
-#     context = {
-#         'times': status,
-#         'day': data,
-#         'rooms': rooms,
-#         'results': results
-#     }
-
-#     return render(request, template, context)
-
-
-# def checkTimeSlot(times, day, rooms, array):
-#     # Collect the timeslots that have not been booked
-#     x = []
-#     for k in times:
-#         # date is the model object and day should be the name to the variable stored from user input
-#         if not (Reservation.objects.filter(date=day, time_slot=k).exists()):
-#             x.append(k)
-#     array.append(x)
-#     return array
-
-
-# def checkAvailability(times, day, rooms):
-#     y = {}
-#     for room in rooms:
-#         time_list = checkTimeSlot(times, day, rooms)
-#         y = add_values_in_dict(y, room.id, time_list)
-        
-#     return y
-
-
-# def add_values_in_dict(sample_dict, key, list_of_values):
-#     ''' Append multiple values to a key in 
-#         the given dictionary '''
-#     if key not in sample_dict:
-#         sample_dict[key] = list()
-#     sample_dict[key].extend(list_of_values)
-#     print(sample_dict)
-#     return sample_dict
