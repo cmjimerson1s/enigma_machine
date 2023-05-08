1c229c08744cafbfd3395495e2028eda804199d2
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-08 16:26:35 +0000

    Update choice view code

--- bookings/views.py
+++ bookings/views.py
@@ -18,7 +18,7 @@ def resultView(request):
     room_list = Room.objects.all()
 
     results = dayView(room_list, specific_date, specific_times)
-
+    choices = []
     context = {
         'results': results,
         'selections': specific_date
@@ -57,18 +57,51 @@ def choiceView(request):
     template = "selected_reservation.html"
     room_data = request.POST['key']
     time_data = request.POST['value']
-
     specific_date = request.POST['selected_date']
+    choice_list = request.POST['choices']
+    array_choice = ReturnArray(choice_list)
     specific_times = GameTime.objects.all()
     room_list = Room.objects.all()
 
     results = dayView(room_list, specific_date, specific_times)
+    reservation = selectionArray(selected_room, selected_time, specific_date)
+
+    if request.method == 'POST':
+        if not any(d == reservation for d in array_choice):
+            array_choice.append(reservation)
+            
+        context = {
+            'reservation': reservation,
+            'choices': array_choice,
+            'results': results,
+            'specific_date': specific_date
+        }
+
+        return render(request, template, context)
 
     context = {
-        'room': room_data,
-        'time': time_data,
+        'reservation': reservation,
+        'choices': array_choice,
         'results': results,
-        'date': specific_date
+        'specific_date': specific_date
     }
 
-    return render(request, template, context)      
+    return render(request, template, context)
+
+def ReturnArray(input):
+    new_input = input
+    result = ast.literal_eval(new_input)
+
+    return result      
+
+def selectionArray(room, time, date):
+    room_data = room
+    time_data = time
+    res_date = date
+    choice_array = []
+    new_dict = {date: {room_data, time_data}}
+
+    if new_dict not in choice_array:
+        choice_array.append(new_dict)
+
+    return choice_array
