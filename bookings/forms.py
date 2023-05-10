155657951f11061edd20ae37517f6548ec8265b3
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-10 11:34:32 +0000

    Removed code to redesign booking app

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .widget import DatePickerInput
 
-class DateForm(forms.Form):
-    selection = forms.DateField(widget=DatePickerInput)
+# class DateForm(forms.Form):
+#     selection = forms.DateField(widget=DatePickerInput)
 
 
-class MyForm(forms.Form):
-    key = forms.HiddenInput()
-    value = forms.HiddenInput()
-    selected_date = forms.HiddenInput()
+# class MyForm(forms.Form):
+#     key = forms.HiddenInput()
+#     value = forms.HiddenInput()
+#     selected_date = forms.HiddenInput()
--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,6 @@
 from . import views
+from django.views import View
 from django.urls import path
 
 urlpatterns = [
-    path('', views.dateChoiceView, name='home'),
-    path('reservation/', views.resultView, name="reservation"),
-    path('reservation_choices/', views.choiceView, name='reservation_picked')
 ]
