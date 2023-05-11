62912da914bdebe2e73425cd5e13aacf2bdaef79
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-11 11:33:25 +0000

    Add view and html for contact form

--- contact/views.py
+++ contact/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def ContactUs(request):
     template = 'contact.html'
+    if request.user.is_authenticated:
+        first_name = request.user.first_name
+        last_name = request.user.last_name
+        email = request.user.email
 
-    return render(request, template)
+    context = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+    }
+
+    return render(request, template, context)
