a7323870daa0fb71979905f5fa5e015e1b3e6ab4
cmjimerson1s <cmjimerson1s@gmail.com>
2023-05-12 11:11:33 +0000

    Update bookings app models

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Reservation, Room, GameTime
 
-admin.site.register(Reservation)
+
 admin.site.register(Room)
 admin.site.register(GameTime)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'date', 'room_choice', 'time_slot')
+    search_fields = ('customer_name',)
+    list_filter = ('room_choice', 'date')
 
--- bookings/models.py
+++ bookings/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.core.exceptions import ValidationError
+
 
 
 
@@ -23,7 +25,7 @@ class Reservation(models.Model):
 
 
 class Room(models.Model):
-    room_name = models.CharField(max_length=200)
+    room_name = models.CharField(max_length=200, unique=True)
     room_description = models.TextField()
     blog_image = CloudinaryField('image', default='placeholder')
 
@@ -35,10 +37,29 @@ class Room(models.Model):
 
 
 class GameTime(models.Model):
-    game_slot = models.CharField(max_length=5)
+    GAME_SLOTS = [
+        ('00:00','00:00'),
+        ('02:00','02:00'),
+        ('04:00','04:00'),
+        ('06:00','06:00'),
+        ('08:00','08:00'),
+        ('10:00','10:00'),
+        ('12:00','12:00'),
+        ('14:00','14:00'),
+        ('16:00','16:00'),
+        ('18:00','18:00'),
+        ('20:00','20:00'),
+        ('22:00','22:00'),
+    ]
+    game_slot = models.CharField(max_length=5, choices=GAME_SLOTS)
 
     class Meta:
         ordering = ['game_slot']
+
+    def clean(self):
+        # Check if there is already a GameTime instance with the same game_slot value
+        if GameTime.objects.filter(game_slot=self.game_slot).exists():
+            raise ValidationError("A game already exists for this time slot.")
 
     def __str__(self):
         return self.game_slot
